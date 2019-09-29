ca2c1b8bff366ddc6ba02045bab007d2c1afa634
Phil Darnowsky <phil@darnowsky.com>
2019-09-29 06:34:18 -0400

    Add first bits of project 1

--- p1/index.js
+++ p1/index.js
@@ -1,6 +1,14 @@
+const chooseTarget = function() {
+  return Math.floor(Math.random() * 100) + 1;
+}
+
 const app = new Vue({
   el: '#app',
   data: {
-    sampleText: 'If you can read this, Vue is wired up correctly.'
+    target: chooseTarget(),
+    nGuessesRemaining: 7,
+    pastGuesses: []
+  },
+  methods: {
   }
 });
