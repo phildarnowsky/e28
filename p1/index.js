be879ead4e8db2cf41a46c23c779cad541bf7293
Phil Darnowsky <phil@darnowsky.com>
2019-10-06 15:07:04 -0400

    Flesh out project 1

--- p1/index.js
+++ p1/index.js
@@ -4,11 +4,65 @@ const chooseTarget = function() {
 
 const app = new Vue({
   el: '#app',
+
   data: {
+    guess: null,
     target: chooseTarget(),
-    nGuessesRemaining: 7,
-    pastGuesses: []
+    maxGuesses: 7,
+    pastGuesses: [],
+    won: false
   },
+
+  computed: {
+    closestHighGuess: function() {
+      const target = this.target;
+      const highGuesses = this.pastGuesses.filter(
+        function(guess) {
+          return guess > target;
+        }
+      );
+
+      if(highGuesses.length == 0) {
+        return null;
+      }
+
+      return(Math.min(...highGuesses));
+    },
+
+    closestLowGuess: function() {
+      const target = this.target;
+      const lowGuesses = this.pastGuesses.filter(
+        function(guess) {
+          return guess < target;
+        }
+      );
+
+      if(lowGuesses.length == 0) {
+        return null;
+      }
+
+      return(Math.max(...lowGuesses));
+    },
+
+    nGuessesRemaining: function() {
+      return this.maxGuesses - this.pastGuesses.length;
+    }
+  },
+
   methods: {
+    makeGuess: function() {
+      if(this.guess == this.target) {
+        this.won = true;
+      } else {
+        this.pastGuesses.push(this.guess);
+        this.guess = null;
+      }
+    },
+
+    resetGame: function() {
+      this.target = chooseTarget();
+      this.pastGuesses = [];
+      this.won = false;
+    }
   }
 });
