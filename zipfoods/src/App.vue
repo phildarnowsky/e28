992384712b5fcd05e0c9d0c262a34f2ab2adf1e9
Phil Darnowsky <phil@darnowsky.com>
2019-11-03 17:57:56 -0600

    Initial ZipFoods

---
+++ zipfoods/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <img alt="ZipFoods logo" src="./images/zipfoods-logo.png" id="logo" />
+    <p>ZipFoods is your one-stop-shop for convenient online grocery shopping in the greater Boston area.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  components: {
+  }
+}
+</script>
+
+<style lang="scss">
+  @import './assets/css/zipfoods.scss';
+</style>
