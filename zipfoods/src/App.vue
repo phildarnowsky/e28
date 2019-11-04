26500b3d00c39e4f74789c720354e0e26b3eb31a
Phil Darnowsky <phil@darnowsky.com>
2019-11-04 06:31:14 -0600

    Add basic ShowFeatured component

--- zipfoods/src/App.vue
+++ zipfoods/src/App.vue
@@ -2,16 +2,18 @@
   <div id="app">
     <img alt="ZipFoods logo" src="./assets/images/zipfoods-logo.png" id="logo" />
     <p>ZipFoods is your one-stop-shop for convenient online grocery shopping in the greater Boston area.</p>
+    <ShowFeatured category="snacks" />
     <ShowProducts />
   </div>
 </template>
 
 <script>
+import ShowFeatured from "./components/ShowFeatured";
 import ShowProducts from "./components/ShowProducts";
 
 export default {
   name: 'app',
-  components: { ShowProducts }
+  components: { ShowFeatured, ShowProducts }
 }
 </script>
 
