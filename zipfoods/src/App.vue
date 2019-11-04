3f1da6bcbe081bf632ade96493d2b56efe67894c
Phil Darnowsky <phil@darnowsky.com>
2019-11-03 18:14:52 -0600

    Basic ZipFoods products page

--- zipfoods/src/App.vue
+++ zipfoods/src/App.vue
@@ -1,15 +1,17 @@
 <template>
   <div id="app">
-    <img alt="ZipFoods logo" src="./images/zipfoods-logo.png" id="logo" />
+    <img alt="ZipFoods logo" src="./assets/images/zipfoods-logo.png" id="logo" />
     <p>ZipFoods is your one-stop-shop for convenient online grocery shopping in the greater Boston area.</p>
+    <ShowProducts />
   </div>
 </template>
 
 <script>
+import ShowProducts from "./components/ShowProducts";
+
 export default {
   name: 'app',
-  components: {
-  }
+  components: { ShowProducts }
 }
 </script>
 
