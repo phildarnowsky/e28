7e7c2916d6bcd5ef88aefca0a659cefb0f7cfcaa
Phil Darnowsky <phil@darnowsky.com>
2019-11-12 03:29:34 -0500

    Build out start of home page

--- p3/src/App.vue
+++ p3/src/App.vue
@@ -6,9 +6,7 @@
 
 <script>
 export default {
-  name: 'app',
-  mounted: function() {
-  }
+  name: 'app'
 }
 </script>
 
--- p3/src/pages/HomePage.vue
+++ p3/src/pages/HomePage.vue
@@ -1,10 +1,35 @@
 <template>
   <div>
-    Home page content goes here.
+    <div v-if="decks">
+      <h1>Available Decks</h1>
+
+      <ul>
+        <li v-for="deck in decks" :key="deck.id">
+          {{ deck.name }}
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
+const axios = require('axios')
+
+export default {
+  name: 'HomePage',
+  data: function() {
+    return({
+      decks: null
+    })
+  },
+  mounted: function() {
+    axios.
+      get('https://my-json-server.typicode.com/phildarnowsky/e28_p3_api/decks').
+      then(response => {
+        this.decks = response.data
+      })
+  }
+}
 </script>
 
 <style lang="scss">
