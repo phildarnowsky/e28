fc4db1c2a25c009ba2e30a559b02dac98280bcce
Phil Darnowsky <phil@darnowsky.com>
2019-11-19 19:35:51 -0500

    Start of a gameboard

---
+++ p3/src/components/Gameboard.vue
@@ -0,0 +1,71 @@
+<script>
+export default {
+  name: 'Gameboard',
+
+  data: function() {
+    return ({
+      guess: null,
+      rightGuessCopy: null,
+      wrongGuessCopy: null
+    })
+  },
+
+  props: {
+    checkGuessCallback: Function,
+    currentCard: Object
+  },
+
+  computed: {
+    validAnswers: function() {
+      const validAnswers = [
+        this.currentCard.canonical_answer,
+        ...this.currentCard.alternate_answers
+      ]
+      const downcasedAnswers = validAnswers.map((answer) => answer.toLowerCase())
+      return new Set(downcasedAnswers)
+    }
+  },
+
+  methods: {
+    checkGuess: function(guess) {
+      if(this.guessIsCorrect(guess)) {
+        this.rightGuessCopy = `Correct! "${this.currentCard.question}" is "${this.currentCard.canonical_answer}"`
+        this.wrongGuessCopy = null
+        this.checkGuessCallback && this.checkGuessCallback();
+      } else {
+        this.rightGuessCopy = null
+        this.wrongGuessCopy = `Sorry, "${guess}" isn't right.`
+        this.guess = '';
+      }
+    },
+
+    guessIsCorrect: function(guess) {
+      const normalizedGuess = guess.trim().toLowerCase()
+      return this.validAnswers.has(normalizedGuess)
+    },
+  }
+}
+</script>
+
+<template>
+  <div>
+    <div v-if={currentCard}>
+      <h2>{{currentCard.question}}</h2>
+      <form @submit.prevent='checkGuess(guess)'>
+        <input type="text" v-model="guess" placeholder="Enter a guess..." />
+        <button type="submit">Guess!</button>
+      </form>
+
+      <div v-if={rightGuessCopy}>
+        {{rightGuessCopy}}
+      </div>
+
+      <div v-if={wrongGuessCopy}>
+        {{wrongGuessCopy}}
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+</style>
--- p3/src/pages/DeckPage.vue
+++ p3/src/pages/DeckPage.vue
@@ -1,26 +1,50 @@
 <script>
+import Gameboard from '../components/Gameboard.vue'
+
 const axios = require('axios')
 
 export default {
   name: 'DeckPage',
+
+  components: {Gameboard},
+
   data: function() {
     return({
+      currentCard: null,
       deck: null
     })
   },
+
   mounted: function() {
     const fetchUrl = 'https://my-json-server.typicode.com/phildarnowsky/e28_p3_api/decks/' + this.$route.params.deckId;
     axios.
       get(fetchUrl).
       then(response => {
-        this.deck = response.data
+        this.deck = response.data;
+        this.chooseCurrentCard();
       })
+  },
+
+  methods: {
+    chooseCurrentCard: function() {
+      const chosenIndex = Math.floor(Math.random() * this.deck.cards.length);
+      this.currentCard = this.deck.cards[chosenIndex];
+    }
   }
 }
 </script>
 
 <template>
   <div>
+    <div v-if="deck">
+      <h1>{{deck.name}}</h1>
+
+      <Gameboard :currentCard='currentCard' />
+    </div>
+
+    <div v-else>
+      Loading...
+    </div>
   </div>
 </template>
 
