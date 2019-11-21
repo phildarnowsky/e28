d1d93666231fb84622f49d1054dc9cdde7e0337a
Phil Darnowsky <phil@darnowsky.com>
2019-11-21 04:48:47 -0500

    Add stats to stats page

--- p3/src/pages/StatsPage.vue
+++ p3/src/pages/StatsPage.vue
@@ -1,9 +1,36 @@
 <script>
+export default {
+  name: 'StatsPage',
+
+  props: {
+    decks: Object
+  },
+
+  methods: {
+    nCompleted: function(deck) {
+      const json = window.localStorage.getItem(deck.id)
+
+      if(json) {
+        return JSON.parse(json).length
+      } else {
+        return 0
+      }
+    },
+
+    nTotal: function(deck) {
+      return deck.cards.length
+    }
+  }
+}
 </script>
 
 <template>
   <div>
     <h1>Stats</h1>
+
+    <p v-for="deck in decks" :key="deck.id">
+      {{deck.name}}: {{nCompleted(deck)}}/{{nTotal(deck)}}
+    </p>
 
     <router-link :to="{name: 'home'}">
       Return to main page
